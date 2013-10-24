175fc082f672a42ace18fad6b4ac0b88e8df6438
Brendan Zhao <crickets147@gmail.com>
2013-10-24 00:30:26 -0400

    Imports are now alphabetically ordered.

--- src/com/brendanzhao/simplecalculator/PortraitCalcActivity.java
+++ src/com/brendanzhao/simplecalculator/PortraitCalcActivity.java
@@ -1,7 +1,7 @@
 package com.brendanzhao.simplecalculator;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
